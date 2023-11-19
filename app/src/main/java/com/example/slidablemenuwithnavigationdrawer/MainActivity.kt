ff0e0699f0e37445e618bbf6458a1391d91a354e
theri <therigasmazurets@gmail.com>
2023-11-19 16:16:52 +0200

    up

---
+++ app/src/main/java/com/example/slidablemenuwithnavigationdrawer/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.slidablemenuwithnavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.slidablemenuwithnavigationdrawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        toggle = ActionBarDrawerToggle(this, binding.drawLayout, R.string.open, R.string.close)
+        binding.drawLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miItem1 -> Toast.makeText(applicationContext,
+                    "Clicked Item 1", Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext,
+                    "Clicked Item 2", Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext,
+                    "Clicked Item 3", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+
+        return super.onOptionsItemSelected(item)
+    }
+}
